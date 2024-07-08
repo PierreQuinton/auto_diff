a72e51e41dd7d2d45e02fe5eb86fc6a13f9a08c1
Hayk <hayk.serobyan@epfl.ch>
2024-07-08 16:57:13 +0200

    Evaluated Ln file

--- src/autodiff/ln.py
+++ src/autodiff/ln.py
@@ -1,5 +1,18 @@
 from autodiff.functions import Function
 from autodiff.product import Product
+import math
 from autodiff.val import Val
 from autodiff.var import Var
 
+class Ln(Function):
+    def __init__(self, var:Var) -> None:
+        self.var=var
+        super().__init__({var})
+        
+
+    def _evaluate(self, values: dict[Var, Val]) -> Val:
+        return Val(math.log(values[self.var]))
+ 
+    
+    def differentiate(self, var:Var) -> Function:
+        raise NotImplementedError
