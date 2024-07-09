43a1199c3c7f3405885fde44a454df3779823526
sbonazzola <sofia.bonazzola@epfl.ch>
2024-07-09 13:48:44 +0200

    division simplified

--- src/autodiff/division.py
+++ src/autodiff/division.py
@@ -1,40 +1,13 @@
-from functions import Function, Var, Val
-from product import Product, Neg
-from sum import Sum
-from power import Power, Inverse
-from compose import Compose
-
-class _Division(Function):
-
-    def __init__(self, numerator: Function, denominator: Function):
-        self.numerator = numerator
-        self.denominator = denominator
-        
-        super().__init__()
-    
-
-    def _evaluate(self, values: dict[Var, Val]) -> Val:
-        num_value = self.numerator._evaluate(values)
-        denom_value = self.denominator._evaluate(values)
-        if denom_value == 0:
-            raise ZeroDivisionError("Division by 0.")
-        return num_value / denom_value
+from functions import Function
+from product import Product
+from power import Inverse
 
 
 
-    def differentiate(self, var: Var) -> Function:
-# (f'g-fg')/g^2
-        f_prime_g = Product([self.numerator.differentiate(var),self.denominator])
-        f_g_prime = Product([self.numerator,self.denominator.differentiate(var)])
-        numerator_diff = Sum([f_prime_g,Neg(f_g_prime)])
-        denominator_squared_inverse= Inverse(Power(2,[self.denominator]))
-        return Product(numerator_diff, denominator_squared_inverse)
+class Division(Product):
 
-
-class Division(Compose):
-
-    def __init__(self, func: Function) -> None:
-        super().__init__(func, _Division())
+    def __init__(self, numerator: Function, denominator: Function) -> None:
+        super().__init__(numerator, Inverse(denominator))
 
 
     def __repr__(self):
