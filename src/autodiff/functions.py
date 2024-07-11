f976676b4006a7913707acd4b94b251f16a2f33e
Hayk <hayk.serobyan@epfl.ch>
2024-07-11 14:36:17 +0200

    Simplify in progress

--- src/autodiff/functions.py
+++ src/autodiff/functions.py
@@ -9,26 +9,21 @@ class Function:
     def __init__(self, funcs: Iterable[Function]) -> None:
         self.funcs = tuple(funcs)
 
-
     # def __apply__(self, values: dict[Var, Val]) -> Val:
     #     if values.keys != self.funcs:
     #         raise ValueError("Wrong keys")
     #     self._substitute(values)
 
-
     def simplify(self) -> Function:
         raise NotImplementedError
-
 
     def substitute(self, substitutions: dict[Function, Function]) -> Function:
         if self in substitutions:
             return substitutions[self]
         return self._substitute(substitutions)
 
-
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         raise NotImplementedError
-
 
     def _evaluate(self, values: dict[Var, Val]) -> Val:
         """
@@ -36,13 +31,11 @@ class Function:
          an evalutation in the already implemented functions.
         """
         raise NotImplementedError
-    
 
     def partial(self, func: Function) -> Function:
         if func not in self.funcs:
             raise ValueError("Cannot take partial derivative wrt independent function")
         return self._partial(func)
-
 
     def _partial(self, func: Function) -> Function:
         """
@@ -50,22 +43,18 @@ class Function:
          an differentiation in the already implemented functions.
         """
         raise NotImplementedError
-    
 
-    def _list_representation(self) -> tuple[str|Function, ...]:
+    def _list_representation(self) -> tuple[str | Function, ...]:
         return (self.__class__.__name__, *self.funcs)
-    
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Function):
             return self._list_representation() == other._list_representation()
         return False
 
-
     def __hash__(self) -> int:
         return self._list_representation().__hash__()
 
-    
     def differentiate(self, var: Var) -> Function:
         simplified_self = self.simplify()
         return simplified_self._differentiate(var).simplify()
@@ -75,8 +64,7 @@ class Function:
             Product([self.partial(func), func._differentiate(var)])
             for func in self.funcs
         ])
-    
-  
+
     def __add__(self, other: Function | float | int):
         if isinstance(other, Function):
             return Sum([self, other])
@@ -84,21 +72,17 @@ class Function:
             return Sum([self, Val(other)])
         elif isinstance(other, int):
             return Sum([self, Val(float(other))])
-    
 
     __radd__ = __add__
 
-
-    def __sub__(self,other: Function | float | int):
+    def __sub__(self, other: Function | float | int):
         if isinstance(other, Function):
             return Substraction(self, other)
         elif isinstance(other, float):
             return Substraction(self, Val(other))
         elif isinstance(other, int):
             return Substraction(self, Val(float(other)))
-       
-    
-    
+
     def __mul__(self, other: Function | float | int):
         if isinstance(other, Function):
             return Product([self, other])
@@ -106,21 +90,18 @@ class Function:
             return Product([self, Val(other)])
         elif isinstance(other, int):
             return Product([self, Val(float(other))])
-    
-    
+
     __rmul__ = __mul__
 
-
-    def __truediv__(self,other: Function | float | int):
+    def __truediv__(self, other: Function | float | int):
         if isinstance(other, Function):
             return Division(self, other)
         elif isinstance(other, float):
             return Division(self, Val(other))
         elif isinstance(other, int):
             return Division(self, Val(float(other)))
-    
-    
-    def __pow__(self,other: Function | float | int):
+
+    def __pow__(self, other: Function | float | int):
         if isinstance(other, Function):
             return Power(self, other)
         elif isinstance(other, float):
@@ -130,25 +111,21 @@ class Function:
 
 
 class Var(Function):
-    
+
     def __init__(self, name: str) -> None:
         self.name = name
         super().__init__({self})
 
-
     def __hash__(self) -> int:
         return self.name.__hash__()
-
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Var):
             return self.name == other.name
         return False
-    
 
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return self
-
 
     def _differentiate(self, var: Var) -> Function:
         if self == var:
@@ -156,14 +133,12 @@ class Var(Function):
         else:
             return Val(0.0)
 
-
     def _partial(self, var: Var) -> Function:
         return Val(1.0)
-    
 
     def __str__(self) -> str:
         return str(self.name)
-    
+
     def simplify(self) -> Function:
         return self
 
@@ -174,28 +149,23 @@ class Val(Function):
         self.val = val
         super().__init__(set())
 
-
     def __hash__(self) -> int:
         return self.val.__hash__()
-
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Val):
             return self.val == other.val
         return False
 
-
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return self
 
-
     def _partial(self, var: Var) -> Function:
         return Val(0.0)
-    
 
     def __str__(self) -> str:
         return str(self.val)
-    
+
     def simplify(self) -> Function:
         return self
 
@@ -209,23 +179,21 @@ def _flatten(functions: Iterable[Function], t: type) -> Counter[Function]:
             funcs.update([func])
     return funcs
 
+
 class Sum(Function):
 
     def __init__(self, functions: Iterable[Function]) -> None:
-        self.func_counter =_flatten(functions, Sum)
+        self.func_counter = _flatten(functions, Sum)
         self.funcs = list(self.func_counter.elements())
         super().__init__(self.funcs)
 
-
     def __hash__(self) -> int:
         return 2 * tuple(self.func_counter.elements()).__hash__()
-
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Sum):
             return self.func_counter == other.func_counter
         return False
-
 
     def simplify(self) -> Function:
         funcs = []
@@ -238,7 +206,7 @@ class Sum(Function):
                 val = func.val
             else:
                 if not isinstance(func, Product):
-                    terms = (func, )
+                    terms = (func,)
                     val = 1.0
                 elif isinstance(func.funcs[0], Val):
                     terms = tuple(func.funcs[1:])
@@ -260,17 +228,14 @@ class Sum(Function):
             return funcs[0]
         return Sum(funcs)
 
-
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         funcs = []
         for func in self.func_counter:
             funcs += [func.substitute(substitutions)] * self.func_counter[func]
         return Sum(funcs)
 
-
     def _partial(self, func: Function) -> Function:
         return Val(1.0)
-
 
     def __str__(self) -> str:
         return "+".join([func.__str__() for func in self.funcs])
@@ -285,19 +250,16 @@ class Substraction(Sum):
 class Product(Function):
 
     def __init__(self, functions: Iterable[Function]):
-        self.func_counter =_flatten(functions, Product)
+        self.func_counter = _flatten(functions, Product)
         super().__init__(list(self.func_counter.elements()))
-
 
     def __hash__(self) -> int:
         return 3 * tuple(self.func_counter.elements()).__hash__()
-
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Product):
             return self.func_counter == other.func_counter
         return False
-
 
     def simplify(self) -> Function:
         funcs = []
@@ -322,13 +284,11 @@ class Product(Function):
             return funcs[0]
         return Product(funcs)
 
-
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         funcs = []
         for func in self.func_counter:
             funcs += [func.substitute(substitutions)] * self.func_counter[func]
         return Product(funcs)
-
 
     def _partial(self, func: Function) -> Function:
         products = []
@@ -339,10 +299,9 @@ class Product(Function):
             else:
                 count = 1
         return Product(products)
-    
 
     def __str__(self) -> str:
-        return "("+")⋅(".join([f.__str__() for f in self.funcs])+")"
+        return "(" + ")⋅(".join([f.__str__() for f in self.funcs]) + ")"
 
 
 class Division(Product):
@@ -352,25 +311,23 @@ class Division(Product):
         self.denominator = denominator
         super().__init__([numerator, Inverse(denominator)])
 
-
     def __str__(self) -> str:
-        return "("+self.numerator.__str__()+")"+"/"+"("+self.denominator.__str__()+")"
+        return "(" + self.numerator.__str__() + ")" + "/" + "(" + self.denominator.__str__() + ")"
 
 
 class Neg(Product):
-    
+
     def __init__(self, function: Function):
         super().__init__([Val(-1), function])
         self.function = function
 
 
 class Exp(Function):
-    
+
     def __init__(self, func: Function) -> None:
         self.func = func
         super().__init__([self.func])
 
-    
     def simplify(self) -> Function:
         func = self.func.simplify()
         if isinstance(func, Val):
@@ -379,17 +336,14 @@ class Exp(Function):
             return func.func
         return Exp(func)
 
-
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return Exp(self.func.substitute(substitutions))
 
-
     def _partial(self, func: Function) -> Function:
         return self
-    
 
     def __str__(self) -> str:
-        return "exp("+self.func.__str__()+")"
+        return "exp(" + self.func.__str__() + ")"
 
 
 class IntegerPower(Function):
@@ -399,21 +353,29 @@ class IntegerPower(Function):
         self.base = base
         super().__init__([base])
 
+    def simplify(self) -> Function:
+        base = self.base.simplify()
+        if isinstance(base, Val):
+            return Val(base.val ** self.exp)
+        elif self.exp == 0:
+            return Val(1.0)
+        elif self.exp == 1:
+            return base
+        return IntegerPower(base, self.exp)
 
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return IntegerPower(self.base.substitute(substitutions), self.exp)
 
-
     def _partial(self, func: Function) -> Function:
         return Product([Val(float(self.exp)), IntegerPower(self.base, self.exp - 1)])
-    
 
     def __str__(self) -> str:
         if self.exp < 0.0:
             exp = "(" + str(self.exp) + ")"
         else:
             exp = str(self.exp)
-        return "("+str(self.base)+")"+"^"+str(exp)
+        return "(" + str(self.base) + ")" + "^" + str(exp)
+
 
 class Power(Exp):
 
@@ -423,9 +385,8 @@ class Power(Exp):
         func = Product([Ln(base), exp])
         super().__init__(func)
 
-    
     def __str__(self) -> str:
-        return "("+self.base.__str__()+")"+"^"+"("+self.exp.__str__()+")"
+        return "(" + self.base.__str__() + ")" + "^" + "(" + self.exp.__str__() + ")"
 
 
 class Inverse(IntegerPower):
@@ -440,7 +401,6 @@ class Ln(Function):
         self.func = func
         super().__init__([self.func])
 
-
     def simplify(self) -> Function:
         func = self.func.simplify()
         if isinstance(func, Val):
@@ -448,15 +408,12 @@ class Ln(Function):
         elif isinstance(func, Exp):
             return func.func
         return Ln(func)
-         
-    
+
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return Ln(self.func.substitute(substitutions))
-    
-    
+
     def _partial(self, func: Function) -> Function:
         return Inverse(self.func)
 
-
     def __str__(self) -> str:
-        return "log("+self.func.__str__()+")"
+        return "log(" + self.func.__str__() + ")"
--- src/autodiff/trig.py
+++ src/autodiff/trig.py
@@ -1,4 +1,5 @@
-from .functions import Function, Neg
+from .functions import Function, Neg, Val
+import math
 
 
 class Cos(Function):
@@ -7,34 +8,39 @@ class Cos(Function):
         self.func = func
         super().__init__([self.func])
 
+    def simplify(self) -> Function:
+        func = self.func.simplify()
+        if isinstance(func, Val):
+            return Val(math.cos(func.val))
+        return Cos(func)
 
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return Cos(self.func.substitute(substitutions))
 
-
     def _partial(self, func: Function) -> Function:
         return Neg(Sin(self.func))
-    
 
     def __str__(self) -> str:
         return "cos(" + self.func.__str__() + ")"
 
-        
+
 class Sin(Function):
 
     def __init__(self, func: Function) -> None:
         self.func = func
         super().__init__([self.func])
 
-        
+    def simplify(self) -> Function:
+        func = self.func.simplify()
+        if isinstance(func, Val):
+            return Val(math.sin(func.val))
+        return Sin(func)
+
     def _substitute(self, substitutions: dict[Function, Function]) -> Function:
         return Sin(self.func.substitute(substitutions))
 
-   
     def _partial(self, func: Function) -> Function:
-           return Cos(self.func)
-
+        return Cos(self.func)
 
     def __str__(self) -> str:
         return "sin(" + self.func.__str__() + ")"
-    
