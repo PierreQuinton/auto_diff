680197dfb100acaffff9481f89b44c199a7f4141
Pierre Quinton <pierre.quinton@epfl.ch>
2024-07-12 12:12:29 +0200

    Improve main

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ x = Var("x")
 y = Var("y")
 z = Var("z")
 # f = Cos(3 * x - Ln(x ** 2 + 1) / Exp(x + x))
-f = x ** 2
+f = (x -4) ** 2 + y ** 2
 f = f.simplify()
-x_0 = {x: 3.0}
+x_0 = {x: 3.0, y: -3.0}
 
-LEgrad = gradient_descent(f, x_0, 0.3)
+LEgrad = gradient_descent(f, x_0, 0.1)
 print(LEgrad)
 print(f.evaluate(LEgrad))
 
